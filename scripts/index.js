8e0e86a586ed3e93d998a82900a0997215307459
Cesar Rosales <cesarrosales1931@gmail.com>
2025-06-26 12:18:00 -0500

    Added index.js and modified index.html

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
