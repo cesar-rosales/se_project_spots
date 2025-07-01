cad92f3f7b453fa17682a0267c157c64a8df5164
Cesar Rosales <cesarrosales1931@gmail.com>
2025-06-30 20:59:05 -0500

    Form Submission and Transitions

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,27 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const linkInput = newPostModal.querySelector("#card-image-input");
+const nameInput = newPostModal.querySelector("#image-caption-input");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileName = document.querySelector(".profile__name");
+const profileBio = document.querySelector(".profile__bio");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileName.textContent;
+  editProfileDescriptionInput.value = profileBio.textContent;
   editProfileModal.classList.add("modal_is-open");
 });
 editProfileCloseBtn.addEventListener("click", function () {
@@ -19,3 +35,19 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-open");
 });
+
+function handelEditProfileSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = editProfileNameInput.value;
+  profileBio.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-open");
+}
+
+function handelAddCardSubmit(event) {
+  event.preventDefault();
+  console.log("image: ", linkInput.value);
+  console.log("caption: ", nameInput.value);
+}
+
+editProfileForm.addEventListener("submit", handelEditProfileSubmit);
+addCardFormElement.addEventListener("submit", handelAddCardSubmit);
